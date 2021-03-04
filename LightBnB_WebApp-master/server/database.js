38dcaee8a26fd78e7a29fed49869b28db5602902
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-04 19:04:35 +0000

    Update getAllReservations to use the lightbnb database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -65,7 +65,17 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  	SELECT properties.*, reservations.*, avg(rating) as average_rating
+		FROM properties
+		JOIN reservations ON properties.id = reservations.property_id
+		JOIN property_reviews ON properties.id = property_reviews.property_id
+		WHERE reservations.guest_id = $1 and end_date < now()::date
+		GROUP BY reservations.id, properties.id
+		ORDER BY start_date
+		LIMIT $2;
+  `, [guest_id, limit])
+    .then(res => res.rows);
 };
 exports.getAllReservations = getAllReservations;
 
